5deeb736331076756f1ae9b687e46e16b8538011
Diya S <60971672+diaas14@users.noreply.github.com>
2022-06-28 02:57:02 +0530

    28062022 binary tree practice

---
+++ 102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,39 @@
+/**
+ * Given the root of a binary tree, return the level order traversal of its nodes' values. 
+ * (i.e., from left to right, level by level).
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> nodesList = new ArrayList<>();
+        if (root == null) return nodesList;
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelNodesList = new ArrayList<>();
+            for (int i=0; i<levelSize; i++) {
+                TreeNode node = queue.poll();
+                levelNodesList.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            nodesList.add(levelNodesList);
+        }
+        return nodesList;
+    }
+}
